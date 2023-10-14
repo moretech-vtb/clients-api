22240ab78057a4f8138ffad4c7e1c01c044c805f
nidrisov <benouylap@gmail.com>
2023-10-14 12:30:25 +0300

    Update office response

--- models/office.py
+++ models/office.py
@@ -18,11 +18,28 @@ class Office(database.Model):
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
     metro_station = Column(String)
-    distance = Column(Integer, nullable=False)
     kep = Column(Boolean, nullable=False)
     my_branch = Column(Boolean, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'address': self.address,
+            'latitude': self.latitude,
+            'longitude': self.longitude,
+            'status': self.status,
+            'rko': self.rko,
+            'office_type': self.office_type,
+            'sale_point_format': self.sale_point_format,
+            'suo_availability': self.suo_availability,
+            'has_ramp': self.has_ramp,
+            'metro_station': self.metro_station,
+            'kep': self.kep,
+            'my_branch': self.my_branch,
+        }
 
 
 class OpenHour(database.Model):
@@ -36,6 +53,13 @@ class OpenHour(database.Model):
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'day_of_week': self.day_of_week,
+            'hours': self.hours,
+        }
+
 
 class DailyLoad(database.Model):
     __tablename__ = 'daily_load'
@@ -44,6 +68,7 @@ class DailyLoad(database.Model):
     office_id = Column(Integer, ForeignKey('offices.id'))
     day_of_week = Column(String(10), nullable=False)
     load_level = Column(Integer, nullable=False)
+    is_individual = Column(Boolean, default=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
--- resources/office.py
+++ resources/office.py
@@ -8,6 +8,33 @@ from utils import authenticate, database
 class OfficesResource(Resource):
 
     @authenticate()
+    def get(self, *args, **kwargs):
+        offices = Office.query.all()
+        open_hours = OpenHour.query.all()
+
+        result_dict = {}
+        result_individual_dict = {}
+        for open_hour in open_hours:
+            if open_hour.is_individual:
+                if open_hour.office_id in result_individual_dict:
+                    result_individual_dict[open_hour.office_id].append(open_hour.serialize())
+                else:
+                    result_individual_dict[open_hour.office_id] = [open_hour.serialize()]
+            else:
+                if open_hour.office_id in result_dict:
+                    result_dict[open_hour.office_id].append(open_hour.serialize())
+                else:
+                    result_dict[open_hour.office_id] = [open_hour.serialize()]
+
+        result = map(lambda x: {
+            **x.serialize(),
+            "open_hours": result_dict.get(x.id, []),
+            "openHoursIndividual": result_individual_dict.get(x.id, [])},
+                     offices)
+
+        return make_response(jsonify(dict(offices=list(result))), 200)
+
+    @authenticate()
     def post(self, *args, **kwargs):
         data = request.json
         session = database.session
@@ -17,39 +44,39 @@ class OfficesResource(Resource):
                 name=entry['salePointName'],
                 address=entry['address'],
                 status=entry['status'],
-                rko=entry['rko'],
+                rko=entry.get('rko') or False,
                 office_type=entry['officeType'],
                 sale_point_format=entry['salePointFormat'],
-                suo_availability=entry['suoAvailability'],
-                has_ramp=entry['hasRamp'],
+                suo_availability=entry.get('suoAvailability') or False,
+                has_ramp=entry.get('hasRamp') or False,
                 latitude=entry['latitude'],
                 longitude=entry['longitude'],
                 metro_station=entry['metroStation'],
-                distance=entry['distance'],
-                kep=entry['kep'],
+                kep=entry.get('kep') or False,
                 my_branch=entry['myBranch'],
-
             )
             session.add(new_office)
             session.flush()
 
             for open_hour in entry['openHours']:
-                new_open_hour = OpenHour(
-                    office_id=new_office.id,
-                    day_of_week=open_hour['days'],
-                    hours=open_hour.get('hours') or 'не обслуживает ЮЛ',
-                    is_individual=False,
-                )
-                session.add(new_open_hour)
+                if open_hour.get('days') != "Не обслуживает ЮЛ":
+                    new_open_hour = OpenHour(
+                        office_id=new_office.id,
+                        day_of_week=open_hour['days'],
+                        hours=open_hour.get('hours') or 'не обслуживает ЮЛ',
+                        is_individual=False,
+                    )
+                    session.add(new_open_hour)
 
             for open_hour_individual in entry['openHoursIndividual']:
-                new_open_hour_individual = OpenHour(
-                    office_id=new_office.id,
-                    day_of_week=open_hour_individual['days'],
-                    hours=open_hour_individual.get('hours') or 'не обслуживает ФЛ',
-                    is_individual=True,
-                )
-                session.add(new_open_hour_individual)
+                if open_hour_individual.get('days') != "Не обслуживает ФЛ":
+                    new_open_hour_individual = OpenHour(
+                        office_id=new_office.id,
+                        day_of_week=open_hour_individual['days'],
+                        hours=open_hour_individual.get('hours') or 'не обслуживает ФЛ',
+                        is_individual=True,
+                    )
+                    session.add(new_open_hour_individual)
 
         session.commit()
-        return make_response(jsonify({'message': 'Added all ATMs to the database'}), 200)
+        return make_response(jsonify({'message': 'Added all Offices to the database'}), 200)
